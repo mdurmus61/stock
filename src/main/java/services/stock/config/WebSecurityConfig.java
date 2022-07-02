1a2f41083ada7aface21edbf6708d2bc56c96b33
mdurmus61 <mustafa.durmus@softtech.com.tr>
2022-07-03 00:35:33 +0300

    test

--- src/main/java/services/stock/config/WebSecurityConfig.java
+++ src/main/java/services/stock/config/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-         http.csrf().disable() 
+         http.csrf().disable()
                 .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/api/user/**").permitAll()
